c8fd70ca8f3c889744a3a9ac7836d0d611037d98
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-07-27 15:06:17 +0700

    Move documentation to doc.go

---
+++ doc.go
@@ -0,0 +1,15 @@
+// gocmt adds missing comments on exported identifiers in Go source files.
+//
+// Usage:
+//
+//  gocmt [-i false|true] [-t "comment template"] [-d dir] [-p false|true]
+//
+// This tools exists because I have to work with some existed code base, which
+// is lack of documentation for many exported identifiers. Iterating over them
+// is time consuming and maybe not suitable at a stage of the project. So I
+// wrote this tool to quickly bypassing CI system. Once thing is settle, we can
+// lookback and fix missing comments.
+//
+// You SHOULD always write documentation for all your exported identifiers.
+
+package main
